ece3a8f1763a86977f9363d560c7b0288b53694a
Tiago Nobrega <tigarmo@gmail.com>
2016-01-18 21:56:26 -0200

    Day two
    Also split each day in a separate module for funsies

---
+++ src/two.rs
@@ -0,0 +1,55 @@
+
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::path::Path;
+use std::cmp::min;
+
+fn sqft(line: &str, accum: &mut i32, bow_accum: &mut i32) {
+    let vec: Vec<&str> = line.split("x").collect();
+
+
+    if vec.len() != 3 {
+        panic!("Could not tokenize line properly: {}", line);
+    }
+
+    let w = vec[0].parse::<i32>().unwrap();
+    let h = vec[1].parse::<i32>().unwrap();
+    let d = vec[2].parse::<i32>().unwrap();
+
+    let wh = w * h;
+    let hd = h * d;
+    let dw = d * w;
+
+    // let min_area = *vec![wh, hd, dw].iter().min().unwrap();
+    let min_area = min(min(wh, hd), dw);
+
+    *accum += 2 * wh + 2 * hd + 2 * dw + min_area;
+
+    let min_per = *vec![w + h, h + d, d + w].iter().min().unwrap() * 2;
+    *bow_accum += min_per + w * h * d;
+}
+
+
+pub fn solve() {
+    let path = Path::new("input-2.txt");
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("could not open file because: {}", Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    let file = BufReader::new(file);
+
+    let mut s = String::new();
+    let mut accum: i32 = 0;
+    let mut bow_accum: i32 = 0;
+
+    for line in file.lines() {
+        sqft(&line.unwrap(), &mut accum, &mut bow_accum);
+    }
+
+    println!("Total area needed: {}", accum);
+    println!("Total bow length needed: {}", bow_accum);
+}
